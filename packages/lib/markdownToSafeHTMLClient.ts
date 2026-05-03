e454241690c72145c316e4764f2ef0214ddf2a14
SahilArate <sahilarate87@gmail.com>
2026-05-03 22:54:02 +0530

    fix: sanitize URL injection and fix ordered/unordered list rendering

--- packages/lib/markdownToSafeHTMLClient.ts
+++ packages/lib/markdownToSafeHTMLClient.ts
@@ -18,19 +18,26 @@ export function markdownToSafeHTMLClient(markdown: string | null): string {
 
     .replace(/`([^`]+)`/g, "<code>$1</code>")
 
+    .replace(/\[([^\]]+)\]\((https?:\/\/[^)]+)\)/g, (_, text, url) => {
+      const safeUrl = encodeURI(url).replace(/'/g, "%27");
+      return `<a target='_blank' class='text-blue-500 hover:text-blue-600' href='${safeUrl}'>${text}</a>`;
+    })
+
+    .replace(/^[*-]\s+(.+)$/gm, "<li class='ul-item'>$1</li>")
+
+    .replace(/^\d+\.\s+(.+)$/gm, "<li class='ol-item'>$1</li>")
+
     .replace(
-      /\[([^\]]+)\]\((https?:\/\/[^)]+)\)/g,
-      "<a target='_blank' class='text-blue-500 hover:text-blue-600' href='$2'>$1</a>"
-    )
-
-    .replace(/^[*-]\s+(.+)$/gm, "<li>$1</li>")
-
-    .replace(/^\d+\.\s+(.+)$/gm, "<li>$1</li>")
-
-    .replace(
-      /(<li>.*<\/li>)/gs,
+      /((?:<li class='ul-item'>[\s\S]*?<\/li>\s*)+)/g,
       "<ul style='list-style-type: disc; list-style-position: inside; margin-left: 12px; margin-bottom: 4px'>$1</ul>"
     )
+
+    .replace(
+      /((?:<li class='ol-item'>[\s\S]*?<\/li>\s*)+)/g,
+      "<ol style='list-style-type: decimal; list-style-position: inside; margin-left: 12px; margin-bottom: 4px'>$1</ol>"
+    )
+
+    .replace(/ class='[ou]l-item'/g, "")
 
     .replace(/\n/g, "<br />");
 
